24f7c5972a8677f574d7cd73b2e6956021a8b50c
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2023-06-23 15:12:39 +0100

    Add effective type specs

--- src/Specs.tsx
+++ src/Specs.tsx
@@ -1,42 +1,42 @@
 import { For, html, $, Observable } from "voby"
-import { Icon } from "./Icon"
 import "./styles/Specs.css"
 
 class Numeric {
   magnitude
-  unit?
+  unit
+  approximate
 
   toString() {
-    if (this.unit) {
-      return `${this.magnitude} ${this.unit}`
-    }
-
-    return `${this.magnitude}`
+    const parts = [this.magnitude.toString()]
+    if (this.unit) parts.push(` ${this.unit}`)
+    if (this.approximate) parts.unshift("~")
+    return parts.join("")
   }
 
-  constructor(magnitude: number, unit?: string) {
+  constructor(magnitude: number, unit?: string, approximate = false) {
     this.magnitude = magnitude
     this.unit = unit
+    this.approximate = approximate
   }
 }
 
-interface HostSpecOptions {
+interface HostSpecOptions<T extends Numeric | string> {
   shortName: string
   id: string
-  getIcon: (value: Numeric | null) => string
-  getValue: () => Numeric | null | undefined
+  getIcon: (value: T | null) => string
+  getValue: (this: this) => T | null | undefined
   isAvailable?: () => boolean
 }
 
 /** Represents a hardware attribute of the host device, e.g. processor clock speed or RAM capacity */
-class HostSpec {
+class HostSpec<T extends Numeric | string = string | Numeric> {
   shortName
   isAvailable
-  getValue: () => Numeric | null | undefined
+  getValue: () => T | null | undefined
   getIcon
   id
 
-  constructor(options: HostSpecOptions) {
+  constructor(options: HostSpecOptions<T>) {
     this.shortName = options.shortName
     this.getValue = options.getValue
     this.isAvailable = options.isAvailable || !!this.getValue()
@@ -46,8 +46,8 @@ class HostSpec {
 }
 
 function Specs(): JSX.Element {
-  const specs: Observable<HostSpec[]> = $([
-    new HostSpec({
+  const specs: Observable<(HostSpec<string> | HostSpec<Numeric>)[]> = $([
+    new HostSpec<Numeric>({
       shortName: "CPU cores",
       id: "cpu-cores",
       getIcon(value) {
@@ -60,13 +60,34 @@ function Specs(): JSX.Element {
         return new Numeric(navigator.hardwareConcurrency)
       },
     }),
-    new HostSpec({
+    new HostSpec<Numeric>({
       shortName: "Ping",
       id: "ping",
       getIcon: () => "mdi:router-network",
       getValue() {
         const ping = navigator.connection?.rtt
-        return ping ? new Numeric(ping, "ms") : null
+        return ping ? new Numeric(ping, "ms", true) : null
+      },
+    }),
+    new HostSpec<string>({
+      shortName: "Network speed category",
+      id: "ping",
+      getIcon() {
+        const effectiveType = navigator.connection?.effectiveType
+        if (effectiveType === "slow-2g") return "mdi:signal-cellular-outline"
+        if (effectiveType === "2g") return "mdi:signal-cellular-1"
+        if (effectiveType === "3g") return "mdi:signal-cellular-2"
+        if (effectiveType === "4g") return "mdi:signal-cellular-3"
+        return "mdi:signal"
+      },
+      getValue() {
+        const effectiveType = navigator.connection?.effectiveType
+        if (!effectiveType) return null
+        if (effectiveType === "slow-2g") return "Very slow"
+        if (effectiveType === "2g") return "Like 2G"
+        if (effectiveType === "3g") return "Like 3G"
+        if (effectiveType === "4g") return "4G or faster"
+        return effectiveType
       },
     }),
   ])
